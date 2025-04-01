b1319bb3ee02ca5b28bf8526b6651f5366219508
Cassiano <bcassiano888@gmail.com>
2025-04-01 20:58:44 -0300

    js alterado

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,2 +1,15 @@
- 
+ function calcular(){
+    // JS nao possui tipo de variável
+    let  mascote, homenagem, doacao, kits
 
+   mascote = document.getElementById("mascote").value;
+
+   homenagem = document.getElementById("homenagem").value;
+
+   doacao = document.getElementById("doacao").value;
+
+   kits = document.getElementById("kits").value;
+
+  let soma = Number(mascote) + Number(homenagem) + Number(doacao)
+  alert(soma)
+ }
