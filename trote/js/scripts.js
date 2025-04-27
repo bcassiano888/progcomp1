6f81fb5717f7645ea467b8f269fdc470e8ae6f7a
Cassiano <bcassiano888@gmail.com>
2025-04-27 15:02:19 -0300

    terminei um pouco do jss

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,39 +1,61 @@
- function calcular(){
-    // JS nao possui tipo de variável
-    let  mascote, homenagem, doacao, kits, soma , equipe
-    //terminar variaveis 
+function calcular(){
+    // vamos criar 2 variáveis 
+    // JS as variáveis não possuem tipo
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe, sangue
+    // recupera o valor do mascote digitado pelo usuário
+    mascote = Number(document.getElementById("mascote").value)
+    // recupera o valor da homenagem digitado pelo usuário
+    homenagem = Number(document.getElementById("homenagem").value)
+    // recupera a qtde de litros de leite
+    leite = Number(document.getElementById("leite").value)
+    // recupera a qtde avulsa de kits de alimentação
+    kit = Number(document.getElementById("kit").value)
+    // recupera a qtde avulsa de suplemento 
+    suplemento = Number(document.getElementById("suplemento").value)
+    // calcular a soma parcial
+    soma = mascote + homenagem + (2 * (leite)) 
+    // vamos calcular a pontuacao considerando metas de kit e supl
+    equipe = document.getElementById("equipe").value
+    // doação de sangue
+    sangue = Number(document.getElementById("sangue").value)
+    if (equipe == "Laranja"){
+        // verifica kit e suplemento
+        if (kit >= 97 && suplemento >= 49) {
+            soma = soma + 5000 + ((kit-97) * 30) + ((suplemento - 49) * 15)
+        }
+        else if (kit >= 78 && suplemento >= 39){
+            soma = soma + 4000 + ((kit-78) * 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >= 25){
+            soma = soma + 2500 + ((kit-49) * 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit >= 19 && suplemento >= 10){
+            soma = soma + 1000 + ((kit-19) * 30) + ((suplemento - 10)* 15)
+        }
+        // verifica sangue
+        if (sangue >= 49){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else {
+            soma = soma + (sangue * 20)
+        }
+    }
+    else if (equipe == "Preta"){
 
-   mascote = Number(document.getElementById("mascote").value)
+    }
+    else if (equipe == "Roxa"){
 
-   homenagem = Number(document.getElementById("homenagem").value)
+    }
+    else if (equipe == "Verde"){
 
-   doacao = Number(document.getElementById("doacao").value)
+    }
+    else { // equipe vermelha
 
-   kits = Number(document.getElementById("kits").value)
+    }
 
-   suplemento = Number(document.getElementById("suplemento").value)
-
-   soma = mascote + homenagem 
-   equipe = document.getElementById("equipe").value
-
-
-  if ( equipe == "laranja"){
-      if(kits >= 97 && suplemento >=49){
-         soma = soma + 5000+((kits-97)*30) + ((suplemento-49)*15)
-      }
-      else if (kits >= 78 && suplemento >= 39){
-         soma = soma + 4000+((kits-78)*30)+((suplemento-39)*15)
-      }
-      else if (kits >= 49 && suplemento >=25){
-
-         soma = soma + 2500+((kits-49)*30)+ ((suplemento-25)*15)
-
-      }
-      else if (kits >= 19  && suplemento >= 10){
-
-         soma = soma + 1000+((kits-19)*30) + ((suplemento-10)*15)
-
-      }
-  }
-  document.getElementById("soma").innerHTML = `A soma é ${soma}`
- }
+    
+    
+    // retorna o valor ao HTML
+    // template string
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
+}
